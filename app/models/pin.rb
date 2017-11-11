f602d199e2aa172c007cf47d8f3fc84c38eb6b68
jthinlay <jthinlay@hotmail.com>
2017-11-11 19:42:41 +0000

    made changes on controller

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -6,3 +6,4 @@ class Pin < ActiveRecord::Base
     # has_attached_file :image, :styles => { :medium => "300X300>", :thumb => "100X100>"}
     # validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
+
